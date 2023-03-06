159c614f32baaecef06ae34c4ad347099cd78ce5
NandanBhat137 <nandanbhat137@gmail.com>
2023-03-06 19:52:03 +0530

    first sh commit

---
+++ first.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+var1 = 123456
+test = rvcemca
+#double quotes
+echo "this is double quote $var1 $test"
+
+#single quote
+echo 'this id single quote demo $var $test'
+
+#reverse quote/ back tick
+echo "this computer / laptop hostname is: `hostname`"
---
+++ second.sh
@@ -0,0 +1,22 @@
+a=10
+b=2
+hostname=$(hostname)
+date=`date`
+1value=333
+false@var=false
+hyphen-a=wrong value
+
+echo "variable a value is :$a"
+echo "variable b value is :$b"
+
+echo "variable hostname value is :$hostname"
+
+echo "variable 1 value value is :$1value"
+
+
+
+echo "variable false value is :$false@var"
+
+
+echo "variable huphen value is :$hyphen-a"
+
