725b7a74b8ec4e0ff4457176f007deb5e3d53272
NandanBhat137 <nandanbhat137@gmail.com>
2023-03-06 20:06:54 +0530

    second sh commit

--- first.sh
+++ first.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-var1 = 123456
-test = rvcemca
+var1=123456
+test=rvcemca
 #double quotes
 echo "this is double quote $var1 $test"
 
 #single quote
-echo 'this id single quote demo $var $test'
+echo 'this id single quote demo '$var1 $test
 
 #reverse quote/ back tick
 echo "this computer / laptop hostname is: `hostname`"
