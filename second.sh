2e0b1c88246d90e9527f3ed0b041dc80d349beae
NandanBhat137 <nandanbhat137@gmail.com>
2023-03-06 20:18:19 +0530

    first sh commit

--- second.sh
+++ second.sh
@@ -2,21 +2,21 @@ a=10
 b=2
 hostname=$(hostname)
 date=`date`
-1value=333
-false@var=false
-hyphen-a=wrong value
+value=333
+falsevar=false
+hyphen_a="wrong value"
 
-echo "variable a value is :$a"
-echo "variable b value is :$b"
+echo "variable a value is :"$a
+echo "variable b value is :"$b
 
 echo "variable hostname value is :$hostname"
 
-echo "variable 1 value value is :$1value"
+echo "variable 1 value value is :$value"
 
 
 
-echo "variable false value is :$false@var"
+echo "variable false value is :$falsevar"
 
 
-echo "variable huphen value is :$hyphen-a"
+echo "variable huphen value is :$hyphen_a"
 
